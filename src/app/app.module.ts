6b4c13c3c247972ad12d64e419418f21cd971618
kasif780780 <kasif780780@gmail.com>
2019-05-06 20:17:12 +0530

    sec-10-4 5. Adding a CanDeactivate route guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MemberDeatilResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter(){
    return localStorage.getItem('token');
@@ -74,7 +75,8 @@ export function tokenGetter(){
       UserService,
       MemberDeatilResolver,
       MemberListResolver,
-      MemberEditResolver
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
