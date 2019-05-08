5181e3a037ec109e7310d76bee9f2e62666d4040
kasif780780 <kasif780780@gmail.com>
2019-05-08 17:45:05 +0530

    11.9. Configuring the 3rd Party file uploader part 2

--- src/app/members/photo-editore/photo-editore.component.ts
+++ src/app/members/photo-editore/photo-editore.component.ts
@@ -25,15 +25,30 @@ export class PhotoEditoreComponent implements OnInit {
     this.hasBaseDropZoneOver = e;
   }
 
-  initializeUploader() {
+  initializeUploader() {  
     this.uploader = new FileUploader({
       url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
-      authToken: 'Bearer' + localStorage.getItem('token'),
-      isHTML5: true;,
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024
     });
+
+    this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false; };
+     this.uploader.onSuccessItem = (item, response, status, Headers) => {
+       if (response) {
+         const res: Photo = JSON.parse(response);
+         const photo = {
+           id: res.id,
+           url: res.url,
+           dataAdded: res.dateAdded,
+           description: res.description,
+           isMain: res.isMain
+        };
+        this.photos.push(photo);
+       }
+     };
   }
 }
